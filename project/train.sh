2765e74cc1e36976c0f3c43b6921df1766927d1c
Rasmus Reimer <rasmus.reimer@gmail.com>
2016-01-11 13:47:34 +0100

    6618

--- project/train.sh
+++ project/train.sh
@@ -2,6 +2,6 @@
 
 TOOLS=./caffe-rc2/build/tools
 
-$TOOLS/caffe train --solver=net/solver1.prototxt --snapshot=snapshots/snapshot_iter_5000.solverstate
+#$TOOLS/caffe train --solver=net/solver1.prototxt --snapshot=snapshots/snapshot_iter_10001.solverstate
 
-#$TOOLS/caffe train --solver=net/solver2.prototxt --snapshot=snapshots/snapshot_iter_4000.solverstate
+$TOOLS/caffe train --solver=net/solver2.prototxt --snapshot=snapshots/snapshot_iter_16000.solverstate
